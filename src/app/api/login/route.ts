41daf3fc743a2519960ccd398ae0c4254865341d
Marcus Gajozo <marcusgajozo@gmail.com>
2024-01-24 13:51:48 -0400

    feat: Cria endpoint de login do usuário

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import prisma from "../../../../lib/db";
+import { PasswordCrypto } from "src/service";
+import { Token } from "src/service";
+
+export async function GET(req: NextRequest) {}
+
+// POST /api/login
+export async function POST(req: NextRequest) {
+  try {
+    const token = req.headers.get("Authorization");
+    const { email, password } = await req.json();
+    const user = await prisma.user.findUnique({ where: { email: email } });
+    if (
+      !user ||
+      !(await PasswordCrypto.comparePassword(password, user.password))
+    ) {
+      return NextResponse.json(
+        { message: "Credentials invalid" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({ message: "OK", user }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
