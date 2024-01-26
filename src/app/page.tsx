fef89fe3ae76d02504c9c7e93e7073b0f278fca9
BuildTools <unconfigured@null.spigotmc.org>
2024-01-26 12:53:39 -0400

    fix: enviar formulário de login ao apertar enter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,12 +29,17 @@ export default function Home() {
     console.log(data);
   };
 
+  function submitForm(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    handleSubmit(handleSignIn)();
+  }
+
   return (
     <>
       <div className={styles.mainLogin}>
         <div className={styles.background}>
           <div className={styles.rightLogin}>
-            <div className={styles.cardLogin}>
+            <form className={styles.cardLogin} onSubmit={submitForm}>
               <h1>LOGIN</h1>
               <div className={styles.textfield}>
                 <label>E-mail</label>
@@ -52,16 +57,13 @@ export default function Home() {
                   {...register("password")}
                 />
               </div>
-              <button
-                className={styles.btnLogin}
-                onClick={() => handleSubmit(handleSignIn)()}
-              >
+              <button type="submit" className={styles.btnLogin}>
                 Entrar
               </button>
               <Link href="/register" style={{ color: "white" }}>
                 Cadastrar-se
               </Link>
-            </div>
+            </form>
           </div>
         </div>
       </div>
