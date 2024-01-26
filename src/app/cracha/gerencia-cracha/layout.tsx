b893dfb614d9b930b0aa4096cb7641dc23cb3993
BuildTools <unconfigured@null.spigotmc.org>
2024-01-26 12:54:02 -0400

    refactor: alterar titulo das paginas 'cracha' e 'gerencia-cracha'

--- src/app/cracha/gerencia-cracha/layout.tsx
+++ src/app/cracha/gerencia-cracha/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Login",
+  title: "Gerenciar crachá",
 };
 
 export default function RootLayout({
--- src/app/cracha/layout.tsx
+++ src/app/cracha/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Login",
+  title: "Crachá",
 };
 
 export default function RootLayout({
